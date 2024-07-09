770f8c0c75ea0f983554764b51c22ec30e713168
Pratik Man Shrestha <160830012+Pratik167@users.noreply.github.com>
2024-07-09 20:14:05 +0545

    Friend function

--- dollar to nepali.cpp
+++ dollar to nepali.cpp
@@ -1,18 +1,43 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void money(float nepali=130, float dollar = 1) 
-{
-    float convert = nepali*dollar;
-    cout << dollar <<"$ = " << convert <<"rupees"<< endl;
-}
-int main() 
-{
-    float dollar;
-    cout << "Enter dollar: ";
-    cin >> dollar;
-    money(130, dollar);
+class Rupees; // Forward declaration
 
+class Dollar
+{
+    private:
+        float D;
+    public:
+        void setdata()
+        {
+            cout << "Enter Dollars:	";
+            cin >> D;
+        }
+        void change(Rupees &r);
+};
+class Rupees
+{
+    private:
+        float Rs;
+    public:
+        void display()
+        {
+            cout << "Equivalent Rupees: Rs_" << Rs << endl;
+        }
+        friend class Dollar;    
+};
+void Dollar::change(Rupees &r)//scope resolutions to define member function outside of the class
+{
+    r.Rs = D * 135.0;
+}
+int main()
+{
+    Rupees r;
+    Dollar d;
+    
+    d.setdata();
+    d.change(r);
+    r.display();
+    
     return 0;
 }
-
